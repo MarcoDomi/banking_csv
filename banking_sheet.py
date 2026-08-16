12a79966059aadaee9dde5091145ed3eb74b4463
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2026-08-16 14:15:43 -0500

    add year to values in DATE column

--- banking_sheet.py
+++ banking_sheet.py
@@ -6,13 +6,20 @@ SERVICE_ACCOUNT_FILE = "intense-acumen-505422-k6-16d08f1e3b27.json"
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 
-def format_sheet(sheet):
-    
-    
-    for data in sheet.get_all_records():
-       if data['DATE'] != '':
-           print(data['DATE'])
+def format_data(sheet):
+    '''format data so it is inline with banking csv sheet'''
+    data = sheet.get_all_records()
 
+    for row in data:
+        if row["DATE"] != "":
+            row["DATE"] += "/2024"
+
+            if (row["AMOUNT"]):
+                pass
+
+        else:
+            pass
+            # move description to previous row
 
 def main():
     creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
@@ -21,12 +28,8 @@ def main():
     SHEET_ID = "1QbJitEyPlr8LuJauisFQEwhNJHd8DGcSknpH_QT5Kds"
     sheet = client.open_by_key(SHEET_ID).sheet1
 
-    row_count = len(sheet.get_all_records())
-    col_count = len(sheet.get_all_records()[0])
+    format_data(sheet)
 
-    #format_sheet(sheet)
-    print(len(sheet.get_all_records()[1]))
-    
 
 if __name__ == "__main__":
     main()
