ac5638251512bd8768ea8d1c821d90cbcfe68343
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2026-08-21 14:31:43 -0500

    setup function to add decimal and zeroes to amount value

--- banking_sheet.py
+++ banking_sheet.py
@@ -5,14 +5,21 @@ from google.oauth2.service_account import Credentials
 SERVICE_ACCOUNT_FILE = "intense-acumen-505422-k6-16d08f1e3b27.json"
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-def update_AMOUNT(row):
+def append_decimal(amount:str):
+    'appends decimal and zeroes to amount if needed'
+    if '.' in amount:
+        print('present')
+
+
+
+def format_AMOUNT(row:dict):
     '''change the value under AMOUNT column to match banking csv format'''
+
     if row["Additions"] != '':
-        row["AMOUNT"] = row["Additions"]
+        row["AMOUNT"] = row["Additions"] #if value present under Additions copy to a cell under AMOUNT
     else:
         row["AMOUNT"] *= -1
-
-    
+    #TODO add decimal and zeroes if needed
 
 
 def format_data(sheet):
@@ -22,8 +29,8 @@ def format_data(sheet):
     for row in data:
         if row["DATE"] != "":
             row["DATE"] += "/2024"
-            update_AMOUNT(row)
-            
+            format_AMOUNT(row)
+            print(row)
         else:
             pass
             # move description to previous row
